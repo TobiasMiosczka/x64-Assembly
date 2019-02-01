929d2d493a7a74ff70b70e235c442d86dbf5c35d
Tobias Miosczka <TobiasMiosczka@users.noreply.github.com>
2019-02-01 04:16:17 +0100

    Update Fibonacci_iterative.asm

--- Fibonacci_iterative.asm
+++ Fibonacci_iterative.asm
@@ -1,15 +1,15 @@
 .code
 main proc
-	mov rax, 1 
+	mov rax, 1       ; result
 	mov rbx, 1
-	mov rcx, 0
+	mov rcx, 0       ; counter
     loopStart: 
 	add rcx, 1
 	add rax, rbx
 	mov rdx, rax
 	mov rax, rbx
 	mov rbx, rdx
-	cmp rcx, 30   ; Change for needed value, in this case n = 30
+	cmp rcx, 30      ; Change for needed value, in this case n = 30
 	jne loopStart
 	ret
 main endp
